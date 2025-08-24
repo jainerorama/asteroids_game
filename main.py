fe2fe7659dcb6b698ec6aff39269ec23c5c47608
jainerorama <jocarralero@gmail.com>
2025-08-24 23:56:47 +0200

    Making the player's ship rotate to left and right

--- main.py
+++ main.py
@@ -18,13 +18,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        dt = time_track.tick(59) / 1000    
         screen.fill((0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = time_track.tick(60) / 1000    
     
     
-    
+        
 
 if __name__ == "__main__":
     main()
