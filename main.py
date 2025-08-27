418ceefaa4e211763afb518473f454343ebf3695
jainerorama <jocarralero@gmail.com>
2025-08-27 23:39:39 +0200

    Updating the code to work with object groups in pygame

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    #GROUPS
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     running = True
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time_track = pygame.time.Clock()
     dt = 0
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
-
+       
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,8 +27,9 @@ def main():
         
         dt = time_track.tick(59) / 1000    
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for objects in drawable:
+            objects.draw(screen)
         pygame.display.flip()
     
     
